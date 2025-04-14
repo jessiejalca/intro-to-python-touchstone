4c6f78e2dee61243bdd14cdec0efd575b9319f8f
Jessie Jalca <jessiejalca@gmail.com>
2025-04-13 22:27:48 -0400

    feat: create a dictionary

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from src import manage_dictionary
 from src import menus
 
 DICT_DIR = "./data"
+DICT_RE = r"[a-z]+_to_[a-z]+\.json"
 
 main_menu_selection = 0
-dictionary_regex = r"[a-z]+_to_[a-z]+\.json"
 dictionary_files = []
 dictionaries = []
 
@@ -22,7 +22,7 @@ while True:
     # add any existing dictionaries
     else:
         files = [f for f in os.listdir(DICT_DIR) if os.path.isfile(os.path.join(DICT_DIR, f))]
-        dictionary_files = [d for d in files if re.match(dictionary_regex)]
+        dictionary_files = [d for d in files if re.match(DICT_RE)]
         for dict_file in dictionary_files:
             dict_name = dict_file[:-5].replace("_", " ")
             main_menu.insert(1, dict_name)
@@ -31,15 +31,15 @@ while True:
     main_menu_selection = menus.print_menu(main_menu)
     
     # Direct user to appropriate menu selection
-    # match main_menu_selection:
-    #     case 1:
-    #         manage_dictionary.create_dictionary(dictionary_regex)
-    #     case len(main_menu):
-    #         print("Exiting flashcard program...")
-    #         break
-    #     case _:
-    #         dictionary_options = ["edit dictionary", "search dictionary", "practice vocabulary", "delete dictionary", "exit dictionary"]
-    #         dict_menu_selection = menus.print_menu(dictionary_options)
+    if main_menu_selection == 1:
+        print("Ok, let's create a new dictionary")
+        manage_dictionary.create_dictionary(DICT_RE)
+    elif main_menu_selection == len(main_menu):
+        print("Exiting flashcard program...")
+        break
+    else:
+        dictionary_options = ["edit dictionary", "search dictionary", "practice vocabulary", "delete dictionary", "exit dictionary"]
+        dict_menu_selection = menus.print_menu(dictionary_options)
     
     break
     
--- src/manage_dictionary.py
+++ src/manage_dictionary.py
@@ -1,10 +1,22 @@
 import re
 import os
 
-def create_dictionary(valid_file):
+def create_dictionary(regex):
     dict_file = ""
-    while not re.match(dict_file, valid_file):
+    while re.match(dict_file, regex):
+        # Get the learner and target languages, and build the dictionary's file name with them
         learner_lang = input("Learner language (the one you're starting from): ")
         target_lang = input("Target language (the one you're learning): ")
-
+        dict_file = f"{learner_lang.lower()}_to_{target_lang.lower()}.json"
         
+        # Check if the dictionary already exists
+        filepath = f"./data/{dict_file}"
+        if os.path.exists(filepath):
+            print("That dictionary already exists.")
+        # If not, create an empty json array
+        else:
+            dictionary = open(filepath, "x")
+            dictionary.write('[]')
+            dictionary.close()
+            print(f"{learner_lang.capitalize()} to {target_lang.capitalize()} dictionary created.\n")
+            
