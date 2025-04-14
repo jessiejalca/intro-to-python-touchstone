d9e6382ebe6cd1b879a9bb4f1f0de0495bc38fb2
Jessie Jalca <jessiejalca@gmail.com>
2025-04-14 10:43:45 -0400

    fix: return user to deck menu to reselect action

--- main.py
+++ main.py
@@ -23,17 +23,18 @@ def print_menu(options):
     print("")
     return menu_selection
 
+def capitalize_list(list):
+    return [item.capitalize() for item in list]
+
 # Loops until user exits
 while True:
     print("\n-------MAIN MENU-------")
     # Create main menu
     # start with basic menu
-    main_menu = ["create new deck", "exit program"]
-    main_menu = [option.capitalize() for option in main_menu]
+    main_menu = capitalize_list(["create new deck", "exit program"])
     if not os.path.exists(DECK_DIR):
         os.mkdir(DECK_DIR)
-    # add any existing decks
-    else:
+    else: # add any existing decks
         decks = [d for d in os.listdir(DECK_DIR) if os.path.isfile(os.path.join(DECK_DIR, d))]
         for deck in decks:
             deck_name = deck[:-5].replace("_", " ").title()
@@ -43,25 +44,34 @@ while True:
     main_menu_selection = print_menu(main_menu)
     
     # Direct user to appropriate main menu selection
-    if main_menu_selection == 1:
-    # Create a deck
+    if main_menu_selection == 1: # Create a deck
         print("Ok, let's create a new deck.")
         manage_deck.create_deck()
-    elif main_menu_selection == len(main_menu):
-    # Exit program
+    elif main_menu_selection == len(main_menu): # Exit program
         print("Exiting flashcard program...\n")
         break
-    else:
-    # Open a deck
-        # print chosen deck name
-        print(f"Opening {main_menu[main_menu_selection - 1]} deck...")
-        
-        # print deck menu
-        deck_options = ["edit deck", "search deck", "practice vocabulary", "delete deck", "exit deck"]
-        deck_options = [option.capitalize() for option in deck_options]
-        deck_menu_selection = print_menu(deck_options)
+    else: # Open a deck
+        # Set deck menu
+        deck_options = capitalize_list(["edit deck", "search deck", "practice session", "delete deck", "exit deck"])
     
-        # direct user to appropriate deck menu selection
-        # match deck_menu_selection:
-        #     case 1:
-                
+        while True:
+            # Print deck name & options
+            print(f"{main_menu[main_menu_selection - 1]} deck")
+            deck_menu_selection = print_menu(deck_options)
+            open_deck = decks[main_menu_selection - 2]
+            
+            # Direct user to appropriate deck menu selection
+            match deck_menu_selection:
+                case 1: # edit deck
+                    print("Ok, which action would you like to take?")
+                    edit_options = capitalize_list(["add cards", "delete cards", "return to deck menu"])
+                    edit_selection = print_menu(edit_options)
+                case 2: # search deck
+                    print("search deck")
+                case 3: # practice session
+                    print("practice session")
+                case 4: # delete deck
+                    print("delete deck")
+                case _:
+                    print("Closing deck...")
+                    break
