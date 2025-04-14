3dfd219f84132985318bf5311638b220e9fc0f03
Jessie Jalca <jessiejalca@gmail.com>
2025-04-14 01:04:42 -0400

    refactor: move print_menu function to main file for simplicity

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 # Language learning flashcard CLI
 
 import os
-import re
 from src import manage_deck
-from src import menus
 
 DECK_DIR = "./data"
 
 main_menu_selection = 0
 decks = []
+
+def print_menu(options):
+    for idx, option in enumerate(options):
+        print(f"({idx + 1}) {option}")
+
+    menu_selection = 0
+    while menu_selection <= 0 or menu_selection > len(options):
+        menu_selection = int(input(f"Select a menu option (1-{len(options)}): "))
+
+    print("")
+    return menu_selection
 
 while True:
     print("\n-------MAIN MENU-------")
@@ -26,15 +35,15 @@ while True:
             main_menu.insert(1, deck_name)
             
     # Print full main menu and get user's selection
-    main_menu_selection = menus.print_menu(main_menu)
+    main_menu_selection = print_menu(main_menu)
     
-    # Direct user to appropriate menu selection
+    # Direct user to appropriate main menu selection
     if main_menu_selection == 1:
-    # create a deck
-        print("Ok, let's create a new deck")
+    # Create a deck
+        print("Ok, let's create a new deck.")
         manage_deck.create_deck()
     elif main_menu_selection == len(main_menu):
-    # exit program
+    # Exit program
         print("Exiting flashcard program...\n")
         break
     else:
@@ -45,6 +54,9 @@ while True:
         # print deck menu
         deck_options = ["edit deck", "search deck", "practice vocabulary", "delete deck", "exit deck"]
         deck_options = [option.capitalize() for option in deck_options]
-        deck_menu_selection = menus.print_menu(deck_options)
+        deck_menu_selection = print_menu(deck_options)
     
-    
+        # direct user to appropriate deck menu selection
+        # match deck_menu_selection:
+        #     case 1:
+                
